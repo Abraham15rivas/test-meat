015abc0e13497686f0091f0a5452aeca6a28e49e
Abraham15rivas <abraham15rivas@gmail.com>
2024-01-16 01:08:55 -0400

    added scss

--- src/components/filter/Filter.vue
+++ src/components/filter/Filter.vue
@@ -1,18 +1,13 @@
 <template>
-  <v-card class="card-filter" >
-    <v-list>
-      <v-list-item
-        v-for="(category, index) in categories"
-        :key="index"
-        link
-        @click="filter(category)"
-      >
-        <v-list-item-title class="text-uppercase">
-          {{ category }}
-        </v-list-item-title>
-      </v-list-item>
-    </v-list>
-  </v-card>
+  <div class="card-filter">
+    <nav class="card-filter__nav">
+      <ul class="card-filter__ul">
+        <li class="card-filter__li" v-for="(category, index) in categories" :key="index" @click="filter(category)">
+          <span class="card-filter__span">{{ category }}</span>
+        </li>
+      </ul>
+    </nav>
+</div>
 </template>
 <script>
   export default {
@@ -29,9 +24,31 @@
     }
   }
 </script>
-<style scoped>
+<style lang="scss" scoped>
   .card-filter {
     border-radius: 10px;
     box-shadow: 0px 4px 40px 0px #00000012;
+
+    &__ul {
+      list-style: none;
+      padding: 0px;
+    }
+
+    &__li {
+      cursor: pointer;
+    }
+
+    &__li:hover {
+      background: rgb(234, 226, 226);
+      border-radius: 10px;
+    }
+
+    &__span {
+      box-sizing: border-box;
+      display: block;
+      width: 100%;
+      padding: 15px 30px;
+      text-transform: uppercase;
+    }
   }
 </style>
